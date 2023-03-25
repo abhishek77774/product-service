830a925120af83239b36e495f7a69cb0f3fbbbbe
Abhishek Sharma <abhishek611sharma@gmail.com>
2023-03-25 15:31:40 +0530

    createProduct method added

---
+++ src/main/java/com/abhishek/productservice/service/ProductService.java
@@ -0,0 +1,26 @@
+package com.abhishek.productservice.service;
+
+import com.abhishek.productservice.dto.ProductRequest;
+import com.abhishek.productservice.model.Product;
+import com.abhishek.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class ProductService {
+    private final ProductRepository productRepository;
+    public void createProduct(ProductRequest productRequest)
+    {
+        Product product = Product.builder()
+                .name(productRequest.getName())
+                .description((productRequest.getDescription()))
+                .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+        log.info("Product {} is saved", product.getId());
+    }
+}
