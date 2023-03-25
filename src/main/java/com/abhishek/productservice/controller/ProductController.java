42eb62ffd65252901ab9f155fdb9faa9f4fc87c7
Abhishek Sharma <abhishek611sharma@gmail.com>
2023-03-25 15:35:39 +0530

    createProduct method completed

--- src/main/java/com/abhishek/productservice/controller/ProductController.java
+++ src/main/java/com/abhishek/productservice/controller/ProductController.java
@@ -1,17 +1,21 @@
 package com.abhishek.productservice.controller;
 
 import com.abhishek.productservice.dto.ProductRequest;
+import com.abhishek.productservice.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/product")
+@RequiredArgsConstructor
 public class ProductController {
 
+    private final ProductService productService;
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createProduct(@RequestBody ProductRequest productRequest)
     {
-
+        productService.createProduct(productRequest);
     }
 }
